fad9160272b22a39b7762cf3665f94366bde3298
nishantbuktare <73768008+nishantbuktare@users.noreply.github.com>
2023-10-11 18:23:22 +0530

    Upload D:\out\webhelp-responsive\oxygen-webhelp\app\search\index\keywords.js

---
+++ webhelp-repo\oxygen-webhelp\app\search\index\keywords.js
@@ -0,0 +1,9 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"ditaPage",p:["p1"]},{w:"file-sample_100kB",p:["p2"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[4];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
